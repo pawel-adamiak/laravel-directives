8db8050e786d50fa93432771a50b7c39fef6f48e
Paweł Adamiak <p.adamiak@tryfon.eu>
2026-09-11 18:09:09 +0200

    [main]

---
+++ src/PAdamiak/LaravelDirectives/ServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+namespace PAdamiak\LaravelDirectives;
+
+use Illuminate\Support\ServiceProvider as SupportServiceProvider;
+use Illuminate\Support\Facades\Blade; // Pamiętaj o imporcie!
+
+class ServiceProvider extends SupportServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        Blade::directive('route', function (string $expression) {
+            return "<?php echo route($expression); ?>";
+        });
+    }
+}
